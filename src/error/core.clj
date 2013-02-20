4c006edf5b82c257c2bdb3ec4c8a3ec34a00e1ef
John Touron <johntouron@gmail.com>
2013-02-19 19:38:40 -0600

    Added if-let+ and IError protocol.

--- src/error/core.clj
+++ src/error/core.clj
@@ -1,6 +1,28 @@
-(ns error.core)
+(ns error.core
+  (:use [clojure.algo.monads :only [defmonad domonad]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defprotocol IError
+  (error? [this]))
+
+(extend-protocol IError
+  Object
+  (error? [_] false)
+  nil
+  (error? [_] true)
+  Throwable
+  (error? [_] true))
+
+(defmonad error-m
+  [m-result identity
+   m-bind   (fn [m f] (if (error? m)
+                        m
+                        (f m)))])
+
+(defmacro if-let+
+  ([bindings return]
+     `(domonad error-m ~bindings ~return))
+  ([bindings return else]
+     `(let [result# (if-let+ ~bindings ~return)]
+        (if (error? result#)
+          ~else
+          result#))))
--- test/error/core_test.clj
+++ test/error/core_test.clj
@@ -2,6 +2,24 @@
   (:use clojure.test
         error.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defrecord TestSuccess []
+  IError
+  (error? [_] false))
+
+(defrecord TestError []
+  IError
+  (error? [_] true))
+
+(deftest if-let+-test
+  (testing "Success"
+    (is (= (if-let+ [a 4] 4) 4))
+    (is (= (if-let+ [a 5 b 3] (+ a b)) 8))
+    (is (= (if-let+ [a "asdf"] "asdf") "asdf"))
+    (is (= (if-let+ [a (TestSuccess.)] (TestSuccess.)) (TestSuccess.))))
+  (testing "Failure"
+    (is (= (if-let+ [a nil] true) nil))
+    (is (= (let [t (Throwable.)] (if-let+ [a t] true) t)))
+    (is (= (if-let+ [a (TestError.)] true) (TestError.))))
+  (testing "Then"
+    (is (= (if-let+ [a nil b 5] true false) false))
+    (is (= (if-let+ [a true b 5] true false) true))))
